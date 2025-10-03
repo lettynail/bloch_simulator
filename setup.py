3f2604ded7d37bd8b5fccc78399664a0e559d774
lettynail <nail.letty@gmail.com>
2025-10-03 20:47:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="bloch_simulator",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "qutip"
-    ],
-)
